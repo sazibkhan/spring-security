a1b871a21aad90cd558b1e72d402937bea121c38
Sazib Khan <sazibkhancse@gmail.com>
2024-06-09 17:46:13 +0600

    CustomPrincipal enty done

--- src/main/java/com/recencysoft/springsecurity/entity/CustomPrincipal.java
+++ src/main/java/com/recencysoft/springsecurity/entity/CustomPrincipal.java
@@ -1,4 +1,62 @@
 package com.recencysoft.springsecurity.entity;
 
-public class CustomPrincipal {
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Getter
+public class CustomPrincipal implements UserDetails{
+
+    private final Long userId;
+    private final String username;
+    private final String fullName;
+    private final UserEntity user;
+
+    public CustomPrincipal(UserEntity user) {
+        this.userId = user.getId();
+        this.username = user.getUsername();
+        this.fullName = user.getFullName();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return user.getRoles().stream()
+                .map(role-> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
 }
